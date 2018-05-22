df7f03600220644729ae26900778cd4f833f3427
Fiorella Cisneros <fiorella.0624@gmail.com>
2018-05-21 19:07:52 -0500

    instanciar variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsComponent } from './components/cards/cards.component';
+
+import { CardsService } from './services/cards.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CardsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/cards/cards.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+ selector: 'app-cards',
+ templateUrl: './cards.component.html',
+ styleUrls: ['./cards.component.scss']
+})
+export class CardsComponent implements OnInit {
+ images = [
+  {id:1, url: "/assets/images/10.png"},
+  {id:2, url: "/assets/images/J.png"},
+  {id:3, url: "/assets/images/K.png"},
+  {id:4, url: "/assets/images/Q.png"}
+ ];
+ images_inact = "/assets/images/poker.png";
+ cards = [];
+ last_select_id = null;
+ aciertos = 4;
+ count_aciertos = 0;
+
+ constructor() { }
+
+ ngOnInit() {
+  let count_index = 0;
+  for(let i = 0; i < this.aciertos * 2; i++){
+   if(count_index == this.aciertos){
+    count_index = 0;
+   }
+   let img = this.images(count_index);
+   this.cards.push({
+    id: img.id, 
+    url: img.url, 
+    visible: false, //si la imagen se muestra
+    active: true //seleccionable
+   });
+   count_index++;
+  }
+
+ }
+
+}
